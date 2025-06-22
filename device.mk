b662b63a7c6057dcdfffa6b9f3472d7e4de5ff90
cooked71 <53344663+cooked71@users.noreply.github.com>
2025-06-22 17:52:24 +0200

    Update device.mk

--- device.mk
+++ device.mk
@@ -10,6 +10,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
+#kernel
+PRODUCT_ENABLE_UFFD_GC := false
+
+
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-impl \
     android.hardware.boot@1.2-impl.recovery \
