89364a19c2ebe02d31ff4f85df73dde9f24856b8
cooked71 <53344663+cooked71@users.noreply.github.com>
2025-07-03 20:51:38 +0200

    Update device.mk

--- device.mk
+++ device.mk
@@ -19,7 +19,7 @@ PRODUCT_ENABLE_UFFD_GC := false
 PRODUCT_COMPATIBILITY_MATRIX_FILE := device/realme/RE58C2/product/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := device/realme/RE58C2/compatibility_matrix.xml
 
-DEVICE_MANIFEST_FILE := device/realme/RE58C2/manifest.xml
+PRODUCT_VENDOR_MANIFEST_FILES := device/realme/RE58C2/manifest.xml
 
 DEVICE_MANIFEST_FILES += \
     vendor/realme/RE58C2/proprietary/vendor/etc/vintf/manifest/ai_engine-default.xml \
