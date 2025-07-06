8a0391bc3e47c458a32e45f1e3ba5513cb06e9e0
cooked71 <53344663+cooked71@users.noreply.github.com>
2025-07-06 02:51:45 +0200

    Update device.mk

--- device.mk
+++ device.mk
@@ -103,18 +103,18 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/root/first_stage_ramdisk/fstab.ums9230_hulk:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.ums9230_hulk \
     $(LOCAL_PATH)/recovery/root/first_stage_ramdisk/fstab.ums9230_nico:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.ums9230_nico\
     $(LOCAL_PATH)/recovery/root/first_stage_ramdisk/fstab.ums9230_zebu:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.ums9230_zebu \
-    $(LOCAL_PATH)/recovery/root/first_stage_ramdisk/fstab.module:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/root/first_stage_ramdisk/fstab.module \
-    $(LOCAL_PATH)/recovery/root/first_stage_ramdisk/fstab.RMX3624:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/root/first_stage_ramdisk/fstab.RMX3624 \
-    $(LOCAL_PATH)/recovery/root/first_stage_ramdisk/fstab.ums9230_1h10:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/root/first_stage_ramdisk/fstab.ums9230_1h10 \
-    $(LOCAL_PATH)/recovery/root/first_stage_ramdisk/fstab.ums9230_1h10_go:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/root/first_stage_ramdisk/fstab.ums9230_1h10_go \
-    $(LOCAL_PATH)/recovery/root/first_stage_ramdisk/fstab.ums9230_4h10:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/root/first_stage_ramdisk/fstab.ums9230_4h10 \
-    $(LOCAL_PATH)/recovery/root/first_stage_ramdisk/fstab.ums9230_4h10_go:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/root/first_stage_ramdisk/fstab.ums9230_4h10_go \
-    $(LOCAL_PATH)/recovery/root/first_stage_ramdisk/fstab.ums9230_6h10:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/root/first_stage_ramdisk/fstab.ums9230_6h10 \
-    $(LOCAL_PATH)/recovery/root/first_stage_ramdisk/fstab.ums9230_7h10:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/root/first_stage_ramdisk/fstab.ums9230_7h10 \
-    $(LOCAL_PATH)/recovery/root/first_stage_ramdisk/fstab.ums9230_haps:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/root/first_stage_ramdisk/fstab.ums9230_haps\
-    $(LOCAL_PATH)/recovery/root/first_stage_ramdisk/fstab.ums9230_hulk:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/root/first_stage_ramdisk/fstab.ums9230_hulk \
-    $(LOCAL_PATH)/recovery/root/first_stage_ramdisk/fstab.ums9230_nico:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/root/first_stage_ramdisk/fstab.ums9230_nico\
-    $(LOCAL_PATH)/recovery/root/first_stage_ramdisk/fstab.ums9230_zebu:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/root/first_stage_ramdisk/fstab.ums9230_zebu 
+    $(LOCAL_PATH)/recovery/root/first_stage_ramdisk/fstab.module:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.module \
+    $(LOCAL_PATH)/recovery/root/first_stage_ramdisk/fstab.RMX3624:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.RMX3624 \
+    $(LOCAL_PATH)/recovery/root/first_stage_ramdisk/fstab.ums9230_1h10:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.ums9230_1h10 \
+    $(LOCAL_PATH)/recovery/root/first_stage_ramdisk/fstab.ums9230_1h10_go:$(TARGET_COPY_OUT_VENDOR_RAMDISK)first_stage_ramdisk/fstab.ums9230_1h10_go \
+    $(LOCAL_PATH)/recovery/root/first_stage_ramdisk/fstab.ums9230_4h10:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.ums9230_4h10 \
+    $(LOCAL_PATH)/recovery/root/first_stage_ramdisk/fstab.ums9230_4h10_go:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.ums9230_4h10_go \
+    $(LOCAL_PATH)/recovery/root/first_stage_ramdisk/fstab.ums9230_6h10:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.ums9230_6h10 \
+    $(LOCAL_PATH)/recovery/root/first_stage_ramdisk/fstab.ums9230_7h10:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.ums9230_7h10 \
+    $(LOCAL_PATH)/recovery/root/first_stage_ramdisk/fstab.ums9230_haps:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.ums9230_haps\
+    $(LOCAL_PATH)/recovery/root/first_stage_ramdisk/fstab.ums9230_hulk:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.ums9230_hulk \
+    $(LOCAL_PATH)/recovery/root/first_stage_ramdisk/fstab.ums9230_nico:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.ums9230_nico\
+    $(LOCAL_PATH)/recovery/root/first_stage_ramdisk/fstab.ums9230_zebu:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.ums9230_zebu 
 
 
 
