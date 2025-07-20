a6b5b7dd2ed9a43d7c78334f2590b7fb7c64c338
cooked71 <53344663+cooked71@users.noreply.github.com>
2025-07-20 06:26:08 +0200

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -9,3 +9,6 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_DEVICE),RE58C2)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
+
+$(call inherit-product, device/realme/RE58C2/recovery/Android.mk)
+
